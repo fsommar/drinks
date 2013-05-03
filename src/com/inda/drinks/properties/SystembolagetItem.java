2ba80e4584c099a25e82aa074cad46e609d356bb
Fred Sommar <fred.sommar+github@gmail.com>
2013-05-03 21:52:38 +0200

    Added variables and constructor in SystembolagetItem.java

--- src/com/inda/drinks/properties/SystembolagetItem.java
+++ src/com/inda/drinks/properties/SystembolagetItem.java
@@ -1,25 +1,32 @@
 package com.inda.drinks.properties;
 
 public class SystembolagetItem {
+	private int id;
+	private int varunummer; // I really don't like mixing languages
+	private double price;
+	private int volume; // in ml
+
+	private SystembolagetItem(int id, int varunummer, double price, int volume) {
+		this.id = id;
+		this.varunummer = varunummer;
+		this.price = price;
+		this.volume = volume;
+	}
 
 	public int getID() {
-		// TODO Auto-generated method stub
-		return 0;
+		return id;
 	}
 
 	public int getVarunummer() {
-		// TODO Auto-generated method stub
-		return 0;
+		return varunummer;
 	}
 
 	public double getPrice() {
-		// TODO Auto-generated method stub
-		return 0;
+		return price;
 	}
 
 	public int getVolume() {
-		// TODO Auto-generated method stub
-		return 0;
+		return volume;
 	}
 
 }
